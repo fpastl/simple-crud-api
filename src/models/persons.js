7234c2f49e9eb7a247c8e1114349fcb21ad3b13c
Stas Smoliar <75983960+fpastl@users.noreply.github.com>
2021-11-27 11:49:46 +0300

    fix: not all fields can be passed for POST

--- src/models/persons.js
+++ src/models/persons.js
@@ -3,9 +3,10 @@ const { v4: uuidv4, validate } = require('uuid');
 
 module.exports = class Persons {
 
-    notFoundID = (id) => { return { message: `not found data with id : ${id}`, status: 404 }};
+    notFoundID = (id) => { return { message: `not found data with id : ${id}`, status: 404 } };
     notPassedValue = (field) => { return { status: 400, message: `field "${field}" not passed` } };
     invalidValue = (field) => { return { status: 400, message: `invalid ${field}` } };
+    hobbiesNotArry = { status: 400, message: "hobbies should be an array" };
 
 
     constructor(bdFile) {
@@ -13,14 +14,14 @@ module.exports = class Persons {
         if (!fs.existsSync(this.bdFile)) {
             fs.writeFileSync(this.bdFile, "[]");
         }
-        try{
+        try {
             this.data = JSON.parse(fs.readFileSync(this.bdFile, "utf8"));
-        }catch{
+        } catch {
             fs.writeFileSync(this.bdFile, "[]");
             console.warn("db file was wrong, it is recreated empty");
             this.data = JSON.parse(fs.readFileSync(this.bdFile, "utf8"));
         }
-        
+
     }
 
     getPerson = (id = false) => {
@@ -38,7 +39,7 @@ module.exports = class Persons {
         if (name === undefined) return this.notPassedValue("name");
         if (age === undefined) return this.notPassedValue("age");
         if (hobbies === undefined) return this.notPassedValue("hobbies");
-        if (!Array.isArray(hobbies)) return { status: 400, message: "hobbies should be an array" };
+        if (!Array.isArray(hobbies)) return hobbiesNotArry;
         return false;
     }
 
@@ -54,39 +55,42 @@ module.exports = class Persons {
             "hobbies": hobbies,
         });
         this.updateBDFile();
-        return { status: 201, data: this.data[this.data.length - 1]};
+        return { status: 201, data: this.data[this.data.length - 1] };
     }
     putPerson = (id, name, age, hobbies) => {
         if (validate(id)) {
-            const notCorrect = this.checkIncorrect(name, age, hobbies);
-            if (notCorrect) return notCorrect;
-            let foundIndex = this.data.findIndex((el) => el["id"] == id);
-            if (foundIndex>=0) {
-                this.data[foundIndex].name = name;
-                this.data[foundIndex].age = age;
-                this.data[foundIndex].hobbies = hobbies;
-                this.updateBDFile();
-                return { status: 200,  data: this.data[foundIndex] };
+            if (name !== undefined || age !== undefined || hobbies !== undefined) {
+                let foundIndex = this.data.findIndex((el) => el["id"] == id);
+                if (foundIndex >= 0) {
+                    if (hobbies !== undefined) {
+                        if (!Array.isArray(hobbies)) return hobbiesNotArry;
+                        this.data[foundIndex].hobbies = hobbies;
+                    }
+                    if (name !== undefined) this.data[foundIndex].name = name;
+                    if (age !== undefined) this.data[foundIndex].age = age;
+                    this.updateBDFile();
+                    return { status: 200, data: this.data[foundIndex] };
+                }
+                return this.notFoundID(id);
             }
-            else return  this.notFoundID(id);
-
+            return { status: 400, message: `no one of data for changes was not transferred` };
         }
         return this.invalidValue("id");
     }
-    deletePerson = (id) =>{
+    deletePerson = (id) => {
         if (validate(id)) {
             let foundIndex = this.data.findIndex((el) => el["id"] == id);
-            if (foundIndex>=0) {
-                this.data.splice(foundIndex,1);
+            if (foundIndex >= 0) {
+                this.data.splice(foundIndex, 1);
                 this.updateBDFile();
                 return { message: `success, delete person with id: "${id}"`, status: 204 };
             }
-            else return  this.notFoundID(id);
+            else return this.notFoundID(id);
         }
         return this.invalidValue("id");
     }
 
-    updateBDFile = ()=>{
+    updateBDFile = () => {
         fs.writeFileSync(this.bdFile, JSON.stringify(this.data));
     }
 }
